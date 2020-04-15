66977a43c8e091fd3abdd7b260c264353cbbce56
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-04-15 20:14:19 +0300

    Cronus:Transport:RabbitMq:Consumer:WorkersCount

--- src/Elders.Cronus.Transport.RabbitMQ/RabbitMqConsumer.cs
+++ src/Elders.Cronus.Transport.RabbitMQ/RabbitMqConsumer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Elders.Cronus.MessageProcessing;
 using Elders.Multithreading.Scheduler;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
@@ -14,24 +13,24 @@ namespace Elders.Cronus.Transport.RabbitMQ
     {
         static readonly ILogger logger = CronusLogger.CreateLogger(typeof(RabbitMqConsumer<>));
 
+        private RabbitMqConsumerOptions options;
         private readonly BoundedContext boundedContext;
-        private readonly int numberOfWorkers;
         private readonly ISubscriberCollection<T> subscriberCollection;
-        private readonly List<WorkPool> pools;
+        private WorkPool pool;
         private readonly ISerializer serializer;
         private readonly IConnectionFactory connectionFactory;
 
-        public RabbitMqConsumer(IConfiguration configuration, IOptionsMonitor<BoundedContext> boundedContext, ISubscriberCollection<T> subscriberCollection, ISerializer serializer, IConnectionFactory connectionFactory)
+        public RabbitMqConsumer(IOptionsMonitor<RabbitMqConsumerOptions> options, IOptionsMonitor<BoundedContext> boundedContext, ISubscriberCollection<T> subscriberCollection, ISerializer serializer, IConnectionFactory connectionFactory)
         {
             if (ReferenceEquals(null, subscriberCollection)) throw new ArgumentNullException(nameof(subscriberCollection));
             if (ReferenceEquals(null, serializer)) throw new ArgumentNullException(nameof(serializer));
 
             this.boundedContext = boundedContext.CurrentValue;
-            numberOfWorkers = configuration.GetValue<int>("cronus_transport_rabbimq_consumer_workerscount", 5);
+            this.options = options.CurrentValue;
+            options.OnChange(OptionsChanged);
             this.subscriberCollection = subscriberCollection;
             this.serializer = serializer;
             this.connectionFactory = connectionFactory;
-            pools = new List<WorkPool>();
         }
 
         protected virtual void ConsumerStart() { }
@@ -46,26 +45,37 @@ namespace Elders.Cronus.Transport.RabbitMQ
             }
 
             ConsumerStart();
-            pools.Clear();
+
+            if (pool is null == false)
+            {
+                logger.Warn($"RabbitMq consumer has already been started with '{options.WorkersCount}' consuments. Returning.");
+                return;
+            }
 
             var poolName = $"cronus: {typeof(T).Name}";
-            WorkPool pool = new WorkPool(poolName, numberOfWorkers);
-            for (int i = 0; i < numberOfWorkers; i++)
+            pool = new WorkPool(poolName, options.WorkersCount);
+            for (int i = 0; i < options.WorkersCount; i++)
             {
                 var consumer = new RabbitMqContinuousConsumer<T>(boundedContext, serializer, connectionFactory, subscriberCollection);
                 pool.AddWork(consumer);
             }
-            pools.Add(pool);
-            pool.StartCrawlers();
 
+            pool.StartCrawlers();
 
             ConsumerStarted();
         }
 
         public void Stop()
         {
-            pools?.ForEach(pool => pool.Stop());
-            pools?.Clear();
+            pool.Stop();
+        }
+
+        private void OptionsChanged(RabbitMqConsumerOptions options)
+        {
+            this.options = options;
+
+            Stop();
+            Start();
         }
     }
 }
