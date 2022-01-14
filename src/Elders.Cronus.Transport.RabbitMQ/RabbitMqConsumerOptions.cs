bf2ab3a47e5bf5f6c71c6b0af79242f3d0ac9c0c
mynkow <mynkow@gmail.com>
2022-01-14 10:27:40 +0200

    Configures the default workers count to 10

--- src/Elders.Cronus.Transport.RabbitMQ/RabbitMqConsumerOptions.cs
+++ src/Elders.Cronus.Transport.RabbitMQ/RabbitMqConsumerOptions.cs
@@ -8,7 +8,7 @@ namespace Elders.Cronus.Transport.RabbitMQ
     public class RabbitMqConsumerOptions : IEquatable<RabbitMqConsumerOptions>
     {
         [Range(1, int.MaxValue, ErrorMessage = "The configuration `Cronus:Transport:RabbitMq:Consumer:WorkersCount` allows values from 1 to 2147483647. For more information see here https://github.com/Elders/Cronus/blob/master/doc/Configuration.md")]
-        public int WorkersCount { get; set; } = 30;
+        public int WorkersCount { get; set; } = 10;
 
         /// <summary>
         /// Drasticly changes the infrastructure behavior. This will create a separate queue per node and a message will be delivered to every node.
